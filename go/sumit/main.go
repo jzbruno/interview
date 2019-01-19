567b8328039741a648782e44b7b8d3aeea9b64c3
Jack Bruno <jzbruno@gmail.com>
2019-01-19 00:47:14 -0700

    Add sum it exercise

---
+++ go/sumit/main.go
@@ -0,0 +1,12 @@
+package main
+
+// Returns an integer that is the sum of the integers between a and b.
+//
+// - If 3 and 5 are passed in the result should be 12: (3 + 4 + 5) = 12.
+// - If 4 and 7 are passed in the result should be 22: (4 + 5 + 6 + 7) = 22.
+//
+// Complete the function below to make the test cases pass.
+//
+func SumIt(a, b int) int {
+	return -1
+}
---
+++ python/sum_it/main.py
@@ -0,0 +1,10 @@
+# Returns an integer that is the sum of the integers between a and b.
+#
+# - If 3 and 5 are passed in the result should be 12: (3 + 4 + 5) = 12.
+# - If 4 and 7 are passed in the result should be 22: (4 + 5 + 6 + 7) = 22.
+#
+# Complete the function below to make the test cases pass.
+
+
+def sum_it(a, b):
+    return -1
